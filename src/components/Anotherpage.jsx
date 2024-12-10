5693a271536c9ca9a2a5703c0dec5422a38e1f43
rizwan3749 <firsindiacredit786@gmail.com>
2024-12-10 17:10:37 +0530

    fix

--- src/components/Anotherpage.jsx
+++ src/components/Anotherpage.jsx
@@ -112,7 +112,7 @@ const Anotherpage = ({ backgroundImage }) => {
           backgroundAttachment: "fixed",
         }}
       >
-        <div className="flex justify-center gap-1 mt-4 -mb-4">
+        <div className="flex justify-center gap-1 mt-2 mb-">
           <button
             onClick={() => setViewMode("grid")}
             className={`p-2 rounded ${
@@ -138,7 +138,7 @@ const Anotherpage = ({ backgroundImage }) => {
         <div
           className={`flex ${
             viewMode === "list" ? "flex-col" : "flex-row"
-          } justify-between mt-2 w-full gap-4`}
+          } justify-between mt-1 w-full gap-4`}
         >
           {viewMode === "grid" ? (
             <>
