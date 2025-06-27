b1479216da32e2311dd66ada2270d3332beb724b
PizeonFly <firsindiacredit786@gmail.com>
2025-06-27 12:15:01 +0530

    Update LandingPage.jsx

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -26,13 +26,13 @@ const BrowserPreview = () => {
 
         {/* Images Container */}
         <div className="relative max-h-[490px]  border border-gray-300/80 border-t-0  rounded-b-xl shadow-md">
-{/*           <video
-            src="/MainHeroVid.webm"
+          <video
+            src="https://res.cloudinary.com/dnjcel8gn/video/upload/v1751005894/MainHeroVid_gwomvb.webm"
             autoPlay
             muted
             loop
             className="w-full h-full object-cover"
-          ></video> */}
+          ></video>
         </div>
       </div>
     </div>
